984019331b0a812f9f3ebf3aa0434c4fea49d1ba
Kelvin Kelxy <kelvinchege265@gmail.com>
2025-10-10 10:21:27 +0300

    Add files via upload

---
+++ UNITS c.C
@@ -0,0 +1,19 @@
+/*
+Name:CHEGE KELVIN MWANGI
+Regestraion number:CT101/G/26475/25
+Description:PROGRAM TO DISPLAY UNITS FOR THIS SEMMESTER
+*/
+#include <stdio.h>
+int main() {
+	
+	printf ("SPC 2101: Introduction to programming and problem solving\n");
+	printf ("SPC 2102: Introduction to computer architecture\n");
+	printf ("UCU 2101: Communication skils\n");
+	printf ("UCU 2105: Fundamentals of ICT\n");
+	printf ("SPM 2100: Discrete Mathematics\n");
+	printf ("SPH 2101: Physics\n");
+	printf ("SPM 2104: Mathematics for Science\n");
+	printf ("SPH 2107: Analogue Electronics");
+	
+	return 0;
+}
