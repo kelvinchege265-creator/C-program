41029592dcd9555358e8dd6381ad7c904eb78d37
Kelvin Kelxy <kelvinchege265@gmail.com>
2025-11-14 14:44:49 +0300

    Add files via upload

---
+++ Fare function.cpp
@@ -0,0 +1,30 @@
+/*
+KELVIN MWANGI CHEGE
+CT101/G/26475/25
+CALCULATES FARE
+
+*/
+#include <stdio.h>
+
+//function that calculates fare
+double calculate_fare(double distance){
+return distance*50;
+}
+
+int main(){
+//variable decleration
+double distance,total;
+
+//rpompt user for info
+printf("enter the distance travllled: ");
+scanf("%lf",&distance);
+
+total=calculate_fare(distance);
+
+printf("the total fare is ksh%.2lf",total);
+
+
+
+    return 0;
+
+}
