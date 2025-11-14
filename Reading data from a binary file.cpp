884ef4a8dab81d4f8f2b9abf4e414db4b1435ffd
Kelvin Kelxy <kelvinchege265@gmail.com>
2025-11-14 14:49:17 +0300

    Add files via upload

---
+++ Reading data from a binary file.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+struct details{
+char name;
+int REG;
+float total_marks;
+
+};
+
+int main(){
+
+// the file must exist in order for it to be opened
+
+FILE *fp;
+fp = fopen("details.bin","rb");
+struct details data;
+
+if(fp == NULL){
+
+    printf("error opening file");
+    return 1;
+
+}
+printf("reading file....\n");
+//raeding  =and displaying 
+
+//read struct in file 
+fread(&data, sizeof(struct details),1,fp);
+
+printf("name:%c\n",data.name);
+printf("REG:%d\n",data.REG);
+printf("total_marks:%.2f\n",data.total_marks);
+
+fclose(fp);
+printf("file closed successfully");
+
+ return 0;
+
+
+}
